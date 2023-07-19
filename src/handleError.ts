61629badb9bbcb41367c7b3ccb98a8ee0b01502b
Mike Marcotte <mike.marcotte@gmail.com>
2023-07-19 16:44:14 -0400

    add test for handling errors

--- src/handleError.ts
+++ src/handleError.ts
@@ -2,22 +2,17 @@ import { ValidationError } from "joi";
 import { UnauthorizedError } from "./errors/unauthorized";
 
 export const handleError = (err: any) => {
-  console.log(`responding with an error`, err);
-  if (err instanceof ValidationError) {
+  console.error(`responding with an error`, err);
+  if (err.statusCode && err.statusCode < 500) {
+    return {
+      statusCode: err.statusCode,
+      body: err.message,
+    };
+  } else if (err instanceof ValidationError) {
     return {
       statusCode: 400,
       body: JSON.stringify(err),
     };
-  } else if (err instanceof UnauthorizedError) {
-    return {
-      statusCode: 403,
-      body: err.message,
-    };
   }
-  return {
-    statusCode: 500,
-    body: JSON.stringify({
-      message: "error!",
-    }),
-  };
+  throw err;
 };
