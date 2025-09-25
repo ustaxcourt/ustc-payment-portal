694960b517058eb4eca7328f801b5cf92a5eb2ce
sranson <sranson@flexion.us>
2025-09-25 09:03:43 -0400

    WIP. Add terraform infastructure for lambda functions.

--- terraform/environments/foundation/dev-networking/main.tf
+++ terraform/environments/foundation/dev-networking/main.tf
@@ -37,3 +37,15 @@ module "iam" {
   }
 }
 
+module "lambda" {
+  source = "../../../modules/lambda"
+  lambda_execution_role_arn       = module.iam.lambda_role_arn
+  subnet_ids                      = module.networking.private_subnet_id
+  security_group_ids              = module.networking.lambda_security_group_id
+  api_gateway_execution_arn       = ""  # Configure thisAPI Gateway is implemented
+
+  tags = {
+    Env     = "dev"
+    Project = "ustc-payment-portal"
+  }
+}
