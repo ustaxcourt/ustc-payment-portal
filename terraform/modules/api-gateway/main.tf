ce625225334e4abe4c288855ddff1b39b34ac194
jcrichlake <jeff.crichlake@agile6.com>
2025-09-25 14:40:45 -0400

    Adding api gateway

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -1,3 +1,18 @@
+data "aws_region" "current"{}
+
+# API Gateway REST API
+resource "aws_api_gateway_rest_api" "main" {
+  name        = "${var.environment}-api-gateway"
+  description = "USTC Payment Payment Portal"
+
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+
+  tags = var.common_tags
+}
+
+# Add api gateway resources 
 
 #These should go in api gateway
 resource "aws_lambda_permission" "init_payment_apigateway" {
