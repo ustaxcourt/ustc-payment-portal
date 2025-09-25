88a4b3a8b643094d8aa52f237ea9223880a6c089
sranson <sranson@flexion.us>
2025-09-25 09:32:52 -0400

    Clarify comment

--- terraform/environments/foundation/dev-networking/main.tf
+++ terraform/environments/foundation/dev-networking/main.tf
@@ -42,7 +42,7 @@ module "lambda" {
   lambda_execution_role_arn       = module.iam.lambda_role_arn
   subnet_ids                      = module.networking.private_subnet_id
   security_group_ids              = module.networking.lambda_security_group_id
-  api_gateway_execution_arn       = ""  # Configure thisAPI Gateway is implemented
+  api_gateway_execution_arn       = ""  # Configure this once API Gateway is implemented
 
   tags = {
     Env     = "dev"
