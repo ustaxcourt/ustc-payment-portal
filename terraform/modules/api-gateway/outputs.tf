b534adf1f22c6d5506d16db8546213f09e776490
Anurag Sharma <asharma@flexion.us>
2025-09-26 08:49:25 -0700

    api gw stuff

---
+++ terraform/modules/api-gateway/outputs.tf
@@ -0,0 +1,15 @@
+output "rest_api_id" {
+  value = aws_api_gateway_rest_api.id
+}
+
+output "api_gateway_execution_arn" {
+    value = aws_api_gateway_rest_api.rest.execution_arn
+}
+
+output "stage_name" {
+    value = aws_api_gateway_stage.stage_name
+}
+
+# output "invoke_url" {
+#     value = 
+# }
