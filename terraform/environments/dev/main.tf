ebacb4383262c04617b0e6ec826d81ffdc4b6cc8
sranson <sranson@flexion.us>
2025-09-26 09:54:57 -0400

    Add versions

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,4 +1,17 @@
 terraform {
+  required_version = ">= 1.7.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
+    }
+  }
+}
+terraform {
   backend "s3" {}
 }
 
