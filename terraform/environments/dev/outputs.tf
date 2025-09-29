0b896ac23d2d1605f8f1658fed72e7d8f13bb12f
Anurag Sharma <asharma@flexion.us>
2025-09-29 12:27:19 -0700

    removed unwanted code

--- terraform/environments/dev/outputs.tf
+++ terraform/environments/dev/outputs.tf
@@ -12,11 +12,3 @@ output "lambda_function_invoke_arns" {
   description = "Map of Lambda function invoke ARNs keyed by function name"
   value       = module.lambda.function_invoke_arns
 }
-
-# output "lambda_role_name" {
-#   value = module.iam.lambda_role_name
-# }
-
-# output "api_gateway_execution_arn" {
-#   value = module.api.api_gateway_execution_arn
-# }
