1d8f7ec94690c4874258d34f9bca9d27d54db734
Anurag Sharma <asharma@flexion.us>
2025-10-08 11:31:14 -0700

    deployed secrets manager to staging

--- terraform/environments/stg/locals.tf
+++ terraform/environments/stg/locals.tf
@@ -1,6 +1,22 @@
 locals {
   aws_region   = "us-east-1"
   environment  = "stg"
+  node_env     = "staging"
+  mtls_enabled = true
+  lambda_env_base = {
+    NODE_ENV                           = local.node_env
+    PAYMENT_URL                        = local.payment_url
+    SOAP_URL                           = local.soap_url
+    API_ACCESS_TOKEN_SECRET_ID         = module.secrets.api_access_token_secret_id
+    CERT_PASSPHRASE_SECRET_ID          = module.secrets.cert_passphrase_secret_id
+    PAY_GOV_DEV_SERVER_TOKEN_SECRET_ID = module.secrets.paygov_dev_server_token_secret_id
+  }
+
+  lambda_env_mtls = local.mtls_enabled ? {
+    PRIVATE_KEY_SECRET_ID = module.secrets.private_key_secret_id
+    CERTIFICATE_SECRET_ID = module.secrets.certificate_secret_id
+  } : {}
+  lambda_env               = merge(local.lambda_env_base, local.lambda_env_mtls)
   github_oidc_provider_arn = "arn:aws:iam::747103385969:oidc-provider/token.actions.githubusercontent.com"
   github_org = "ustaxcourt"
   github_repo = "ustc-payment-portal"
@@ -12,5 +28,7 @@ locals {
   ]
   lambda_exec_role_arn = "arn:aws:iam::747103385969:role/ustc-payment-portal-stg-lambda-exec"
   name_prefix = "ustc-payment-processor-stg"
+  payment_url          = "https://pay-gov-dev.ustaxcourt.gov/pay"
+  soap_url             = "https://pay-gov-dev.ustaxcourt.gov/wsdl"
 }
 
