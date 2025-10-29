c92084ef3863f7d4ce8c050e1ddde5910e7e81d7
Anurag Sharma <asharma@flexion.us>
2025-10-29 12:55:09 -0700

    cross account tf error

--- terraform/modules/artifacts_bucket/outputs.tf
+++ terraform/modules/artifacts_bucket/outputs.tf
@@ -1,10 +1,10 @@
 output "bucket_name" {
-  value = aws_s3_bucket.build_artifacts.id
+  value       = aws_s3_bucket.build_artifacts.id
   description = "Build artifacts bucket ID"
 }
 
 output "bucket_arn" {
-  value = aws_s3_bucket.build_artifacts.arn
+  value       = aws_s3_bucket.build_artifacts.arn
   description = "ARN for build artifacts bucket"
 }
 
