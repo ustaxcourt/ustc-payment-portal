8b2e0061247698bbb6ea6cee15c54627a62238cb
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2026-02-12 12:32:08 -0800

    PAY-049: Update to teraform

--- terraform/environments/dev/versions.tf
+++ terraform/environments/dev/versions.tf
@@ -9,10 +9,6 @@ terraform {
       source  = "hashicorp/archive"
       version = "~> 2.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
   }
 }
 terraform {
