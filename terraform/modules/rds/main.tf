d3acee456e7c94008f4e582134248f58a93eaab5
ben-eccles-gunnison <beccles@gunnison.biz>
2026-02-18 12:32:53 -0700

    Typo fix, adds cloud watch logging to RDS

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -1,3 +1,32 @@
+resource "aws_db_parameter_group" "main" {
+  name   = "${var.identifier}-pg"
+  family = "postgres16"
+
+  parameter {
+    name  = "log_statement"
+    value = "all"
+  }
+
+  parameter {
+    name  = "log_min_duration_statement"
+    value = "1000" # Log queries taking more than 1 second
+  }
+
+  parameter {
+    name  = "log_connections"
+    value = "1"
+  }
+
+  parameter {
+    name  = "log_disconnections"
+    value = "1"
+  }
+
+  tags = {
+    Name = "${var.identifier}-parameter-group"
+  }
+}
+
 resource "aws_db_instance" "main" {
   identifier     = var.identifier
   engine         = "postgres"
@@ -6,6 +35,7 @@ resource "aws_db_instance" "main" {
   instance_class    = var.instance_class
   allocated_storage = var.allocated_storage
   storage_encrypted = true
+  storage_type      = "gp3"
 
   db_name  = var.db_name
   username = var.username
@@ -15,10 +45,14 @@ resource "aws_db_instance" "main" {
   vpc_security_group_ids = var.vpc_security_group_ids
   publicly_accessible    = false
 
+  parameter_group_name = aws_db_parameter_group.main.name
+
   backup_retention_period = var.backup_retention_period
   multi_az                = var.multi_az
 
   deletion_protection       = var.deletion_protection
   skip_final_snapshot       = var.skip_final_snapshot
+
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
   final_snapshot_identifier = var.final_snapshot_identifier
 }
