f6443b0841fc26c3e6783fbac8602a276b9304ef
Anthony <Anthony.Loera.ctr@ustaxcourt.gov>
2026-02-18 12:46:08 -0800

    PAY-049: Update Recovery window to use variable instead of hard coding

--- terraform/environments/prod/secrets-module.tf
+++ terraform/environments/prod/secrets-module.tf
@@ -1,9 +1,10 @@
 module "secrets" {
-  source               = "../../modules/secrets"
-  environment          = local.environment
-  lambda_exec_role_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.name_prefix}-lambda-exec"
-  enable_mtls          = true
-  rds_secret_name      = "rds-credentials"
+  source                  = "../../modules/secrets"
+  environment             = local.environment
+  recovery_window_in_days = 30
+  lambda_exec_role_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${local.name_prefix}-lambda-exec"
+  enable_mtls             = true
+  rds_secret_name         = "rds-credentials"
   tags = {
     Project = "ustc-payment-portal"
     Env     = local.environment
