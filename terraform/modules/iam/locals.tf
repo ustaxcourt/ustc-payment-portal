07098b770464efc4f762925d737e8b7c8b16c7ec
ben-eccles-gunnison <beccles@gunnison.biz>
2026-03-02 15:06:12 -0700

    adds missing permissions

--- terraform/modules/iam/locals.tf
+++ terraform/modules/iam/locals.tf
@@ -4,17 +4,17 @@ locals {
 
   github_oidc_provider_arn = var.github_oidc_provider_arn
 
-  tf_state_bucket_name  = var.state_bucket_name
-  tf_lock_table_name    = var.state_lock_table_name
-  state_object_keys     = var.state_object_keys
+  tf_state_bucket_name = var.state_bucket_name
+  tf_lock_table_name   = var.state_lock_table_name
+  state_object_keys    = var.state_object_keys
 
-  lambda_exec_role_arn  = var.lambda_exec_role_arn
-  name_prefix    = var.name_prefix
-  lambda_name_prefix = var.create_deployer_role ? "${var.project_name}-${var.environment}" : ""
+  lambda_exec_role_arn = var.lambda_exec_role_arn
+  name_prefix          = var.name_prefix
+  lambda_name_prefix   = var.create_deployer_role ? "${var.project_name}-${var.environment}" : ""
 
-  aws_region    = var.aws_region
-  project_name  = var.project_name
-  environment   = var.environment
+  aws_region       = var.aws_region
+  project_name     = var.project_name
+  environment      = var.environment
   deploy_role_name = var.deploy_role_name
 }
 
