9416f7aa60f47244372993f9591a6d4ae5c02b85
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-03-09 07:54:56 -0700

    PAY-070 feat: Get count from payload

--- web-client/src/features/transactions/hooks/useTransactionByStatus.ts
+++ web-client/src/features/transactions/hooks/useTransactionByStatus.ts
@@ -1,9 +1,10 @@
 import { useFetch } from '../../../lib/hooks/useFetch'
 import { fetchTransactionsByStatus } from '../api/transactions.api'
-import type { PaymentStatus, Transaction } from '../types'
+import type { TransactionsResponse } from '../api/transactions.api'
+import type { PaymentStatus } from '../types'
 
 export function useTransactionsByStatus(status: PaymentStatus) {
-  return useFetch<Transaction[]>(
+  return useFetch<TransactionsResponse>(
     (signal) => fetchTransactionsByStatus(status, { signal }),
     [status]
   )
