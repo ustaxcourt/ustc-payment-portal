cefbf41a229764b729f933b3225f4395ef53f176
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-03-11 09:44:42 -0700

    clean: Sync with main

--- src/entities/StartOnlineCollectionRequest.ts
+++ src/entities/StartOnlineCollectionRequest.ts
@@ -1,10 +1,16 @@
+import Joi from "joi";
 import { RawStartOnlineCollectionRequest } from "../types/RawStartOnlineCollectionRequest";
 import { AppContext } from "../types/AppContext";
 import { StartOnlineCollectionResponse } from "../types/StartOnlineCollectionResponse";
 import { RequestType, SoapRequest } from "./SoapRequest";
-import { StartOnlineCollectionSchema } from "../schemas";
 
-export const startOnlineCollectionSchema = StartOnlineCollectionSchema;
+export const startOnlineCollectionSchema = Joi.object({
+  agencyTrackingId: Joi.string().required(),
+  tcsAppId: Joi.string().required(),
+  transactionAmount: Joi.number().required(),
+  urlCancel: Joi.string().required(),
+  urlSuccess: Joi.string().required(),
+});
 
 export type StartOnlineCollectionRequestParams = {
   tcs_app_id: string;
