d86d4627065ec8116203e97306a00c910ac6a287
jeanobame11 <jeanobame99@gmail.com>
2026-03-16 16:37:06 -0500

    added z.coerce

--- src/schemas/StartOnlineCollection.schema.ts
+++ src/schemas/StartOnlineCollection.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const StartOnlineCollectionSchema = z.object({
   tcsAppId: z.string(),
   agencyTrackingId: z.string(),
-  transactionAmount: z.number(),
+  transactionAmount: z.coerce.number(),
   urlCancel: z.string(),
   urlSuccess: z.string(),
 });
