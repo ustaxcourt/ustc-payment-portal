6b5fe0ae06f97f244e5a6d73eee93e4453fc7473
ben-eccles-gunnison <beccles@gunnison.biz>
2026-03-18 15:16:42 -0400

    Makes empty base_path explicit

--- terraform/modules/api-gateway/main.tf
+++ terraform/modules/api-gateway/main.tf
@@ -241,6 +241,7 @@ resource "aws_api_gateway_base_path_mapping" "mapping" {
   api_id      = aws_api_gateway_rest_api.rest.id
   stage_name  = aws_api_gateway_stage.stage.stage_name
   domain_name = aws_api_gateway_domain_name.custom[0].domain_name
+  base_path = ""
 }
 
 resource "aws_route53_record" "custom_domain" {
