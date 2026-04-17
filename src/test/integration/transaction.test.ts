20530c98c637978a21ffdea423df2e5a1e47646b
ben-eccles-gunnison <beccles@gunnison.biz>
2026-04-17 13:33:17 -0400

    indent fix

--- src/test/integration/transaction.test.ts
+++ src/test/integration/transaction.test.ts
@@ -96,6 +96,6 @@ describe("make a transaction", () => {
 
     const data = await result.json();
     expect(data.trackingId).toBe(payGovTrackingId);
-      expect(data.transactionStatus).toBe("processed");
+    expect(data.transactionStatus).toBe("processed");
   });
 });
