7663e4643d2327aa2554347f98d532eda75a88b4
ben-eccles-gunnison <beccles@gunnison.biz>
2026-04-17 14:28:24 -0400

    missed a space

--- src/useCases/parseTransactionStatus.test.ts
+++ src/useCases/parseTransactionStatus.test.ts
@@ -10,7 +10,7 @@ describe("parseTransactionStatus", () => {
   );
 
   it.each(["Failed", "Cancelled", "Retired"] as PayGovTransactionStatus[])(
-    "Returns 'failed' forFailed, Cancelled, and Retired transaction statuses",
+    "Returns 'failed' for Failed, Cancelled, and Retired transaction statuses",
     (transactionStatus: PayGovTransactionStatus) => {
       expect(parseTransactionStatus(transactionStatus)).toBe("failed");
     }
