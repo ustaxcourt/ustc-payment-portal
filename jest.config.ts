8452ac1412e450907aee6fb72c20e20024b7a5e9
Franz Noel Tanglao <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-05-13 12:26:28 -0700

    chore: Jest config and remove test output

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,13 @@ const config: Config = {
   preset: "ts-jest",
   testEnvironment: "node",
   setupFiles: ["dotenv/config"],
+  bail: true,
+  testPathIgnorePatterns: ["/node_modules/", "/test/integration/", "/dist/"],
+  coveragePathIgnorePatterns: [
+    "/node_modules/",
+    "/dist/",
+    "src/test/testAppContext.ts",
+  ],
 };
 
 export default config;
