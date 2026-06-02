d6cde1030e572abfe2bd633984cb9c5a330f9977
jeanobame11 <jeanobame99@gmail.com>
2026-06-01 22:43:45 -0500

    PAY-264: Added gh actions read only role

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -394,3 +394,34 @@ resource "aws_iam_role_policy" "github_actions_permissions" {
     ]
   })
 }
+
+# General-purpose read-only CI role. Currently assumed by the terraform-plan workflow on PRs;
+# safe for any future CI use case that only needs to inspect AWS state.
+resource "aws_iam_role" "github_actions_read_only" {
+  count = var.create_deployer_role ? 1 : 0
+  name  = local.read_only_role_name
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "GithubOIDCAssumeRole"
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = local.github_oidc_provider_arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = local.github_sub
+          }
+        }
+      }
+    ]
+  })
+}
+
+
