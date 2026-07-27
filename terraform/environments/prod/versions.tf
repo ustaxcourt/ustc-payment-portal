92a76bb2e8ea7aa75ccfeeb49b00d24bf8c39c5e
Franz Noel <Franz.Tanglao.ctr@ustaxcourt.gov>
2026-07-27 11:59:07 -0700

    PAY-412 dependency updates (Week of 2026-07-20) (#328)
    * PAY-412 feat: Update package.json
    
    * PAY-412 feat: Add update dependency scripts
    
    * PAY-412 feat: Add Dependency review and update in package.json scripts
    
    * PAY-412 docs: Rename Markdown for managing dependencies
    
    * PAY-412 feat: Update Terraform lock files for DEV and STG
    
    * PAY-412 feat: Deleted the scripts and docs for updating dependencies
    
    * PAY-412 feat: Update prod Terraform locks
    
    * PAY-412 feat: Remove unavailable scripts for dependency review and update
    
    * PAY-412 feat: Update AWS SDK
    
    * PAY-412 feat: Update AWS, Smithy, and Node Types packages
    
    * PAY-412 feat: Update Terraform provider versions
    
    * PAY-412 feat: Update changesets
    
    * PAY-412 chore: Update description of changeset update
    
    * PAY-412 feat: Downgrade to 24.13.3
    
    * PAY-412 docs: Add dependency caveat @types/node
    
    * PAY-412 feat: Update accepted vulnerabilities section
    
    * PAY-412 feat: Update AWS SDK to v3.1095.0
    
    * PAY-412 docs: Updated Dependency Caveat
    
    * PAY-412 docs: Update dependency caveats 2027-07-20
    
    * PAY-412 docs: Remove image in Dpendency Caveats

--- terraform/environments/prod/versions.tf
+++ terraform/environments/prod/versions.tf
@@ -3,15 +3,15 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 6.0"
     }
     archive = {
       source  = "hashicorp/archive"
-      version = "~> 2.0"
+      version = "~> 2.8"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.0"
+      version = "~> 3.9"
     }
   }
   backend "s3" {}
