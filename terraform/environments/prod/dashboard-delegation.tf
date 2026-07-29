1911ff19adfa250dea56c835eaea58de22b17cda
AnthonyLoeraTaxCourt <Anthony.Loera.ctr@ustaxcourt.gov>
2026-07-29 06:16:06 -0700

    PAY-330 delegate dev-dashboard subdomain from payments.ustaxcourt.gov… (#339)
    * PAY-330 delegate dev-dashboard subdomain from payments.ustaxcourt.gov to the dashboard dev account
    
    * PAY-330: use local.custom_domain for prod dev-dashboard NS delegation record name
    
    * PAY-330: add prevent_destroy lifecycle to prod dev-dashboard NS delegation record for safety

---
+++ terraform/environments/prod/dashboard-delegation.tf
@@ -0,0 +1,22 @@
+# Delegates dashboard subdomains out of payments.ustaxcourt.gov to the account
+# hosting each dashboard. NS values come from that environment's
+# `dashboard_zone_name_servers` output in ustc-payment-portal-dashboard, and go
+# stale if the zone there is ever destroyed and recreated.
+
+resource "aws_route53_record" "dashboard_dev_delegation" {
+  zone_id = aws_route53_zone.this.zone_id
+  name    = "dev-dashboard.${local.custom_domain}"
+  type    = "NS"
+  ttl     = 172800
+
+  records = [
+    "ns-1006.awsdns-61.net",
+    "ns-1378.awsdns-44.org",
+    "ns-1641.awsdns-13.co.uk",
+    "ns-320.awsdns-40.com",
+  ]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
