c8917dbe1bd08edcfc43ebd3b9b4f5c8ea652d53
ben-eccles-gunnison <beccles@gunnison.biz>
2026-08-07 14:22:32 -0400

    Merge in main

--- src/db/TransactionModel.ts
+++ src/db/TransactionModel.ts
@@ -6,9 +6,6 @@ import type { TransactionStatus as SchemaTransactionStatus } from "@schemas/Tran
 import type { Knex } from "knex";
 import { Model } from "objection";
 import { getActiveFee } from "../config/fees";
-import type { Knex } from "knex";
-import { Model } from "objection";
-import { getActiveFee } from "../config/fees";
 import { getKnex } from "./knex";
 
 export type TransactionStatus = SchemaTransactionStatus;
